d2c0c9646ab61bcdca54013aa81b839f8e184d7e
Steve Leach <steve.leach@yahoo.com>
2016-03-12 11:18:24 +0000

    Penalty now reports end time

--- src/org/steveleach/scoresheet/model/PenaltyEvent.java
+++ src/org/steveleach/scoresheet/model/PenaltyEvent.java
@@ -31,6 +31,14 @@ public class PenaltyEvent extends GameEvent {
 
     @Override
     public String toString() {
-        return String.format("%s - %s %s %dm (%s) for %s", gameTime, team, eventType, minutes, subType, player);
+        return String.format("%s - %s %s %dm (%s) for %s (%s)", gameTime, team, eventType, minutes, subType, player, finishTime());
+    }
+
+    private String finishTime() {
+        String startTime = getGameTime();
+        int mins = Integer.parseInt(startTime.substring(0,2));
+        int secs = Integer.parseInt(startTime.substring(3,5));
+        mins += getMinutes();
+        return String.format("%02d:%02d",mins,secs);
     }
 }
